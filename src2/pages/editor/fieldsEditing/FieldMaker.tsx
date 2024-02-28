469e105d8ef94fafe027eac5201dfe8050d26b0f
Ahmad <ahmadhassan0381@gmail.com>
2024-02-28 14:00:26 +0500

    impoved UI

--- src2/pages/editor/fieldsEditing/FieldMaker.tsx
+++ src2/pages/editor/fieldsEditing/FieldMaker.tsx
@@ -1,17 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { TFields } from "../../../types/FormObject";
 import { MapTypeToCompoenet } from "./interface";
-import {
-  THtmlProps,
-  TList,
-  TNumber,
-  TRequired,
-} from "../../../types/TypeBasedProps";
 import RequiredInputs from "./required/RequiredInputs";
 import OptionalProperties from "./optionalProperties/OptionalProperties";
 import HtmlProperties from "./htmlProperties/HtmlProperties";
 import Collapsible from "../../../compoenents/Collapsible";
-import Sidebar from "../../../compoenents/Sidebar";
+import { useChangeMode, useActions, useDoneFields } from "./fieldMakerLogic";
 
 const FieldMaker = () => {
   const [singleField, setSingleField] = useState<TFields>({
@@ -19,205 +13,143 @@ const FieldMaker = () => {
     fieldName: "",
     fieldType: "",
   });
-  const [inputMode, setinputMode] = useState({
-    required: true,
-    typeBased: false,
-    optional: false,
-    HtmlProps: false,
-  });
-
-  const ChangeMode = {
-    required: () => {
-      let x = {
-        required: true,
-        typeBased: false,
-        optional: false,
-        HtmlProps: false,
-      };
-      setinputMode(x);
-    },
-    typeBased: () => {
-      let x = {
-        required: false,
-        typeBased: true,
-        optional: false,
-        HtmlProps: false,
-      };
-      setinputMode(x);
-    },
-    optional: () => {
-      let x = {
-        required: false,
-        typeBased: false,
-        optional: true,
-        HtmlProps: false,
-      };
-      setinputMode(x);
-    },
-    HtmlProps: () => {
-      let x = {
-        required: false,
-        typeBased: false,
-        optional: false,
-        HtmlProps: true,
-      };
-      setinputMode(x);
-    },
-  };
-  const Actions = {
-    requiredFields: {
-      Next: (object: TRequired) => {
-        let xx = { ...singleField };
-        object.id ? (xx.id = object.id) : null;
-        object.fieldName ? (xx.fieldName = object.fieldName) : null;
-        object.fieldType ? (xx.fieldType = object.fieldType) : null;
-        setSingleField(xx);
-        ChangeMode.typeBased();
-      },
-    },
-    typeBased: {
-      Number: {
-        HandlePropsApply: (object: TNumber) => {
-          let x = { ...singleField };
-          object.numberMin ? (x.numberMin = object.numberMin) : null;
-          object.numberMax ? (x.numberMax = object.numberMax) : null;
-          object.numberDecimal
-            ? (x.numberDecimal = object.numberDecimal)
-            : null;
-          object.validation ? (x.validation = object.validation) : null;
-          setSingleField(x);
-          ChangeMode.HtmlProps();
-        },
-        SkipHandle: () => {
-          ChangeMode.HtmlProps();
-        },
-      },
-      List: {
-        handlePropsApply: (object: TList) => {
-          let x = { ...singleField };
-          x.options = object.options;
-          object.data ? (x.data = object.data) : null;
-          setSingleField(x);
-          ChangeMode.HtmlProps();
-        },
-      },
-    },
-    HtmlProps: {
-      Apply: (object: THtmlProps) => {
-        let x = { ...singleField };
-        x.visible = object.visible ? true : false;
-        x.enable = object.enable ? true : false;
-        x.required = object.required ? true : false;
-        setSingleField({ ...x });
-        ChangeMode.optional();
-      },
-      SkipHandle: () => {
-        ChangeMode.optional();
-      },
-    },
-  };
+  const { doneFields, setDone } = useDoneFields();
+  const { inputMode, ChangeMode } = useChangeMode();
+  const Actions = useActions();
 
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column", gap: "15px" }}>
       <Collapsible
         isOpen={inputMode.required}
         onClick={() => {
           if (!inputMode.required) ChangeMode.required();
         }}
-        title="this is a collapsable"
+        title="Required Fields"
       >
-        <div>
-          <RequiredInputs
-            onNext={Actions.requiredFields.Next}
-            RequiredProperties={{
-              id: singleField.id,
-              fieldName: singleField.fieldName,
-              fieldType: singleField.fieldType,
-            }}
-          />
-        </div>
-      </Collapsible>
-
-      {/* {inputMode.required && (
         <RequiredInputs
-          onNext={Actions.requiredFields.Next}
+          onNext={(object) =>
+            Actions.requiredFields.Next(
+              object,
+              singleField,
+              setSingleField,
+              ChangeMode,
+              setDone
+            )
+          }
           RequiredProperties={{
             id: singleField.id,
             fieldName: singleField.fieldName,
             fieldType: singleField.fieldType,
           }}
         />
-      )} */}
+      </Collapsible>
 
-      {inputMode.typeBased &&
-        (() => {
-          switch (singleField.fieldType) {
-            case "number":
-              return (
-                <>
-                  <h2>Type Based Properties</h2>
-                  <MapTypeToCompoenet.number
-                    // NumberFieldsSchema={{
-                    //   numberDecimal: singleField.numberDecimal,
-                    //   numberMax: singleField.numberMax,
-                    //   numberMin: singleField.numberMin,
-                    //   validation: singleField.validation,
-                    // }}
-                    onApplyProperties={
-                      Actions.typeBased.Number.HandlePropsApply
-                    }
-                    onSkipProperties={Actions.typeBased.Number.SkipHandle}
-                  />
-                </>
-              );
-            case "list":
-            case "radioList":
-              return (
-                <>
-                  <h2>Type Based Properties</h2>
-                  <MapTypeToCompoenet.list
-                    ListFieldsProps={{
-                      options: singleField.options ? singleField.options : [],
-                      data: singleField.data ? singleField.data : [],
-                    }}
-                    onApply={Actions.typeBased.List.handlePropsApply}
-                  />
-                </>
-              );
+      {doneFields.req && (
+        <Collapsible
+          isOpen={inputMode.typeBased}
+          title="TypeBased"
+          onClick={() => {
+            if (!inputMode.typeBased) ChangeMode.typeBased();
+          }}
+        >
+          {inputMode.typeBased &&
+            (() => {
+              switch (singleField.fieldType) {
+                case "number":
+                  return (
+                    <>
+                      <h2>Type Based Properties</h2>
+                      <MapTypeToCompoenet.number
+                        onApplyProperties={(object) =>
+                          Actions.typeBased.Number.HandlePropsApply(
+                            object,
+                            singleField,
+                            setSingleField,
+                            ChangeMode,
+                            setDone
+                          )
+                        }
+                        onSkipProperties={() =>
+                          Actions.typeBased.Number.SkipHandle(
+                            ChangeMode,
+                            setDone
+                          )
+                        }
+                      />
+                    </>
+                  );
+                case "list":
+                case "radioList":
+                  return (
+                    <>
+                      <h2>Type Based Properties</h2>
+                      <MapTypeToCompoenet.list
+                        ListFieldsProps={{
+                          options: singleField.options
+                            ? singleField.options
+                            : [],
+                          data: singleField.data ? singleField.data : [],
+                        }}
+                        onApply={(object) =>
+                          Actions.typeBased.List.handlePropsApply(
+                            object,
+                            singleField,
+                            setSingleField,
+                            ChangeMode,
+                            setDone
+                          )
+                        }
+                      />
+                    </>
+                  );
 
-            default:
-              ChangeMode.HtmlProps();
-              return null;
-          }
-        })()}
-
-      {inputMode.HtmlProps && (
-        <HtmlProperties
-          onApply={Actions.HtmlProps.Apply}
-          onSkip={Actions.HtmlProps.SkipHandle}
-        />
+                default:
+                  ChangeMode.HtmlProps();
+                  setDone("type");
+                  return null;
+              }
+            })()}
+        </Collapsible>
       )}
-      {inputMode.optional && <OptionalProperties />}
+
+      {doneFields.type && (
+        <Collapsible
+          isOpen={inputMode.HtmlProps}
+          title="HTML Props"
+          onClick={() => {
+            if (!inputMode.HtmlProps) ChangeMode.HtmlProps();
+          }}
+        >
+          {inputMode.HtmlProps && (
+            <HtmlProperties
+              onApply={(object) =>
+                Actions.HtmlProps.Apply(
+                  object,
+                  singleField,
+                  setSingleField,
+                  ChangeMode,
+                  setDone
+                )
+              }
+              onSkip={() => Actions.HtmlProps.SkipHandle(ChangeMode, setDone)}
+            />
+          )}
+        </Collapsible>
+      )}
+
+      {doneFields.html && (
+        <Collapsible
+          title="Optional"
+          isOpen={inputMode.optional}
+          onClick={() => {
+            if (!inputMode.optional) ChangeMode.optional;
+          }}
+        >
+          {inputMode.optional && <OptionalProperties />}
+        </Collapsible>
+      )}
     </div>
   );
 };
 
 export default FieldMaker;
-
-// const abc = {
-//   numberDecimal: true,
-//   numberMin: 4,
-//   numberMax: 10,
-//   validation: {
-//     rules: [{ type: "w", rule: "abc", msg: "abc", color: "red" }],
-//   },
-// };
-// const req = {
-//   id: "req",
-//   fieldName: "abc",
-//   fieldType: "number",
-// };
-// const l1: TList = {
-//   options: ["abc", "bcd"],
-//   data: ["aa1", "aa2"],
-// };
---
+++ src2/pages/editor/fieldsEditing/fieldMakerLogic.ts
@@ -0,0 +1,167 @@
+import { useState } from "react";
+import {
+  TNumber,
+  TList,
+  TRequired,
+  THtmlProps,
+} from "../../../types/TypeBasedProps";
+import { TFields } from "../../../types/FormObject";
+
+interface doneFields {
+  req: boolean;
+  type: boolean;
+  opt: boolean;
+  html: boolean;
+}
+
+export const useChangeMode = () => {
+  const [inputMode, setInputMode] = useState({
+    required: true,
+    typeBased: false,
+    optional: false,
+    HtmlProps: false,
+  });
+
+  const ChangeMode = {
+    required: () => {
+      let x = {
+        required: true,
+        typeBased: false,
+        optional: false,
+        HtmlProps: false,
+      };
+      setInputMode(x);
+    },
+    typeBased: () => {
+      let x = {
+        required: false,
+        typeBased: true,
+        optional: false,
+        HtmlProps: false,
+      };
+      setInputMode(x);
+    },
+    optional: () => {
+      let x = {
+        required: false,
+        typeBased: false,
+        optional: true,
+        HtmlProps: false,
+      };
+      setInputMode(x);
+    },
+    HtmlProps: () => {
+      let x = {
+        required: false,
+        typeBased: false,
+        optional: false,
+        HtmlProps: true,
+      };
+      setInputMode(x);
+    },
+  };
+
+  return { inputMode, ChangeMode };
+};
+
+export const useDoneFields = () => {
+  const [doneFields, setDoneFields] = useState<doneFields>({
+    req: false,
+    type: false,
+    opt: false,
+    html: false,
+  });
+
+  const setDone = (key: "req" | "type" | "opt" | "html") => {
+    setDoneFields({ ...doneFields, [key]: true });
+  };
+
+  return { doneFields, setDone };
+};
+
+export const useActions = () => {
+  const Actions = {
+    requiredFields: {
+      Next: (
+        object: TRequired,
+        singleField: TFields,
+        setSingleField: React.Dispatch<React.SetStateAction<TFields>>,
+        ChangeMode: any,
+        setDone: any
+      ) => {
+        let xx = { ...singleField };
+        object.id ? (xx.id = object.id) : null;
+        object.fieldName ? (xx.fieldName = object.fieldName) : null;
+        object.fieldType ? (xx.fieldType = object.fieldType) : null;
+        setSingleField(xx);
+        ChangeMode.typeBased();
+        setDone("req");
+      },
+    },
+    typeBased: {
+      Number: {
+        HandlePropsApply: (
+          object: TNumber,
+          singleField: TFields,
+          setSingleField: React.Dispatch<React.SetStateAction<TFields>>,
+          ChangeMode: any,
+          setDone: any
+        ) => {
+          let x = { ...singleField };
+          object.numberMin ? (x.numberMin = object.numberMin) : null;
+          object.numberMax ? (x.numberMax = object.numberMax) : null;
+          object.numberDecimal
+            ? (x.numberDecimal = object.numberDecimal)
+            : null;
+          object.validation ? (x.validation = object.validation) : null;
+          setSingleField(x);
+          ChangeMode.HtmlProps();
+          setDone("type");
+        },
+        SkipHandle: (ChangeMode: any, setDone: any) => {
+          ChangeMode.HtmlProps();
+          setDone("type");
+        },
+      },
+      List: {
+        handlePropsApply: (
+          object: TList,
+          singleField: TFields,
+          setSingleField: React.Dispatch<React.SetStateAction<TFields>>,
+          ChangeMode: any,
+          setDone: any
+        ) => {
+          let x = { ...singleField };
+          x.options = object.options;
+          object.data ? (x.data = object.data) : null;
+          setSingleField(x);
+          ChangeMode.HtmlProps();
+          setDone("type");
+        },
+      },
+    },
+    HtmlProps: {
+      Apply: (
+        object: THtmlProps,
+        singleField: TFields,
+        setSingleField: React.Dispatch<React.SetStateAction<TFields>>,
+        ChangeMode: any,
+        setDone: any
+      ) => {
+        let x = { ...singleField };
+        x.visible = object.visible ? true : false;
+        x.enable = object.enable ? true : false;
+        x.required = object.required ? true : false;
+        setSingleField({ ...x });
+        ChangeMode.optional();
+        setDone("html");
+      },
+      SkipHandle: (ChangeMode: any, setDone: any) => {
+        ChangeMode.optional();
+        setDone("html");
+      },
+    },
+  };
+
+  return Actions;
+};
